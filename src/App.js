54453646b7d86bcc318c682281137052a3af75d0
dev-kokje <dev-kokje@gmail.com>
2021-09-13 22:16:57 +0530

    code refactoring

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 
 function App() {
+
+  const expenses = [
+    {
+      id: 1,
+      title: 'Car Insurance',
+      date: new Date(2021, 11, 10),
+      amount: 150
+    },
+    {
+      id: 2,
+      title: 'Petrol',
+      date: new Date(2021, 11, 24),
+      amount: 100.5
+    },
+    {
+      id: 3,
+      title: 'Ganpati Decoration',
+      date: new Date(2021, 10, 10),
+      amount: 60.55
+    },
+    {
+      id: 4,
+      title: 'Bike Repair',
+      date: new Date(2021, 10, 8),
+      amount: 200
+    }
+  ];
+
   return (
     <div>
-      <ExpenseItem />
-      <ExpenseItem />      
+      <Expenses expenses={expenses} />      
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -8,6 +8,7 @@ const styles = makeStyles({
         display: 'block',
         backgroundColor: '#eee',
         border: '1px solid #bbb',
+        margin: '10px',
     },
     price_block: {
         display: 'inline-block',
@@ -18,23 +19,20 @@ const styles = makeStyles({
     }
 });
 
-function ExpenseItem() {
+const ExpenseItem = (props) => {
 
     const classes = styles();
-    const expenseDate = new Date(2021, 11, 1);
-    const expenseTitle = "Luggage Bags";
-    const expensePrice = 200.45;
 
     return (
         <Card className={classes.root}>
             <CardContent>
                 <Box display="flex" flexDirection="Cols">
                     <Box flexGrow={1}>
-                        <Box fontSize={20} fontWeight="fontWeightBold">{expenseTitle}</Box>
-                        <Box>{expenseDate.toLocaleDateString()}</Box>
+                        <Box fontSize={20} fontWeight="fontWeightBold">{props.data.title}</Box>
+                        <Box>{props.data.date.toLocaleDateString()}</Box>
                     </Box>
                     <Box>
-                        <Button variant="contained" color="primary">$ {expensePrice}</Button>
+                        <Button variant="contained" color="primary" size="large">$ {props.data.amount}</Button>
                     </Box>
                 </Box>
             </CardContent>
